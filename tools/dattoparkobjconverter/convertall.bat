5e5ad75ee5e1098dafcbf2bd3aae97c48fec46c5
Karst <karstvangalenlast@gmail.com>
2022-07-21 18:50:46 -0500

    Convert to PNGs instead of directly to parkobjs

--- tools/dattoparkobjconverter/convertall.bat
+++ tools/dattoparkobjconverter/convertall.bat
@@ -4,19 +4,19 @@ echo "Start conversion DAT --> parkobj (optimized)"
 
 echo "Rides:"
 title "Converting Rides (Dat --> parkobj (optimized))"
-for /r %%i in (DAT\Rides\*) do objexport %%i parkobj\Rides\ -z
+for /r %%i in (DAT\Rides\*) do objexport %%i parkobj\Rides\ --png
 
 echo "Paths:"
 title "Converting Paths (Dat --> parkobj (optimized))"
-for /r %%i in (DAT\Paths\*) do objexport %%i parkobj\Paths\ --split -z
+for /r %%i in (DAT\Paths\*) do objexport %%i parkobj\Paths\ --split --png
 
 echo "Scenery:"
 title "Converting Scenery (Dat --> parkobj (optimized))"
-for /r %%i in (DAT\Scenery\*) do objexport %%i parkobj\Scenery -z
+for /r %%i in (DAT\Scenery\*) do objexport %%i parkobj\Scenery --png
 
 echo "Themes:"
 title "Converting Themes (Dat --> parkobj (optimized))"
-for /r %%i in (DAT\Themes\*) do objexport %%i parkobj\Themes -z
+for /r %%i in (DAT\Themes\*) do objexport %%i parkobj\Themes --png
 
 echo "Done converting..."
 title "Finshed conversion (Dat --> parkobj (optimized))"
